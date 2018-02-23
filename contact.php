8a8edfb81e2de86162fc7a20a693bd272d379abc
LamiaRimty <rimtycse@gmail.com>
2018-02-22 16:55:56 -0800

    displaying contact

--- contact.php
+++ contact.php
@@ -11,12 +11,12 @@
 
 		<!-- content section -->
 		<div class="content">
-				<div class="floatl"><h1>John Doe</h1></div>
+				<div class="floatl"><h1>Labonno</h1></div>
 				<a class="floatr" href="index.php"><input class="cancel_contact_button" type="button" value="Home"></a>
 				<div class="clear"></div>
 				<hr>
 				<div class="contact">
-							<img src="img/default_profile_pic.jpg" alt="default image"  width="40%" style="float:left;">
+							<img src="img/Labonno.jpg" alt="default image"  width="40%" style="float:left;">
 					<div class="contact_info">
 						<p><b>Nickname:</b> </p>
 						<p><b>Cell Phone:</b> </p>
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 require_once"connection.php";
-$all_contacts = "select * form contacts";
-
+ $all_contacts = "select * from contacts";
+ 
+ $sql_all_contacts = $conn-> query($all_contacts);
+ 
+ $total_contacts =$sql_all_contacts->num_rows;
 
 
 ?>
@@ -21,7 +24,7 @@ $all_contacts = "select * form contacts";
 
 		<!-- content section -->
 		<div class="content">
-			<div class="floatl"><h1>Number of contacts here</h1></div>
+			<div class="floatl"><h1><?php echo $total_contacts?> contacts in phonebook</h1></div>
 			<a class="floatr" href="insert_contact.php"><input class="cancel_contact_button" type="button" value="New Contact"></a>		
 			<div class="clear"></div>
 			<hr class="pageTitle">
@@ -32,16 +35,23 @@ $all_contacts = "select * form contacts";
 						<th>Nickname:</th>
 						<th>Cell Phone:</th>
 						<th>Actions</th>
-					</tr>
+					</tr>   
 				</thead>
 				<tbody>
-					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
+					<?php while ( $row= mysqli_fetch_assoc($sql_all_contacts) ){
+						# code...
+		
+					 ?>
+
+                      <tr>
+						<td><a href="contact.php"><?php echo $row['contact_fname'] ." " . $row['contact_lname'] ?></a></td>
+						<td><?php echo $row['contact_nickname'] ?></td>
+						<td><?php echo $row['contact_cphone'] ?></td> 
+
+
 						<td><i class="fa fa-pencil"></i> | <i class="fa fa-trash-o"></i></td>
 					</tr>
-					
+					<?php } ?>
 				</tbody>
 			</table>
 		</div>
